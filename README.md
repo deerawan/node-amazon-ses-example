018dc1240495fe3fd36ed239e92b0eb72310d3b4
Budi Irawan <deerawan@gmail.com>
2015-09-02 23:02:16 +0700

    add license, readme and update index.js

---
+++ README.md
@@ -0,0 +1,14 @@
+Node Amazon SES Example
+================================
+An example of sending email using Amazon SES. More explanation (http://budiirawan.com/send-emails-using-amazon-ses-and-node-js/)[http://budiirawan.com/send-emails-using-amazon-ses-and-node-js/]
+
+## Install
+```
+npm install
+```
+
+## Run
+Please change some email addresses in **index.js**. You can also comment parts of sending email to test it.
+```
+node index.js
+```
--- index.js
+++ index.js
@@ -11,15 +11,15 @@ var transporter = nodemailer.createTransport(ses({
 /** Simple Email */
 transporter.sendMail({
     from: 'noreply@youremail.com',
-    to: 'destination@gmail.com',
+    to: 'destination@youremail.com',
     subject: 'My Amazon SES Simple Email',
     text: 'Amazon SES is cool'
   });
 
-/** Email with CC and BCC */
+/** Email with CC and BCC. */
 transporter.sendMail({
     from: 'noreply@youremail.com',
-    to: 'destination@gmail.com',
+    to: 'destination@youremail.com',
     subject: 'My Amazon SES Email with CC and BCC',
     text: 'Amazon SES CC and BCC is cool',
     cc: 'superganteng@yopmail.com, supertampan@yopmail.com',
@@ -29,7 +29,7 @@ transporter.sendMail({
 /** Email with Attachment */
 transporter.sendMail({
   from: 'noreply@youremail.com',
-  to: 'destination@gmail.com',
+  to: 'destination@youremail.com',
   subject: 'My Amazon SES Email with Attachment',
   text: 'Amazon SES Attachment is cool',
   attachments: [
